c2a2535b6f8da97b454b23fb8c396574fe98562a
Diego Bustos Kehdy <diegobustos19@hotmail.com>
2024-02-19 16:24:11 -0300

    DataBase mockup

---
+++ backend/database/Tables.sql
@@ -0,0 +1,80 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE Users (
+    user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Categories (
+    category_id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+CREATE TABLE Products (
+    product_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    product_name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    stock INT,
+    category_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+);
+
+CREATE TABLE ProductImages (
+    image_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    product_id UUID NOT NULL,
+    image_url TEXT NOT NULL,
+    alt_text TEXT,
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+
+CREATE TABLE Orders (
+    order_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(50),
+    total_amount DECIMAL(10, 2), 
+    payment_status VARCHAR(50), 
+    tracking_info TEXT, 
+    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+);
+
+CREATE TABLE OrderDetails (
+    order_details_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    order_id UUID,
+    product_id UUID,
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+CREATE TABLE Carts ( 
+    cart_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    user_id UUID,
+    product_id UUID,
+    quantity INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+CREATE TABLE Favorites (
+    user_id UUID,
+    product_id UUID,
+    favorited_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    PRIMARY KEY (user_id, product_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+
+CREATE INDEX idx_users_email ON Users(email);
+CREATE INDEX idx_products_name ON Products(product_name);
+CREATE INDEX idx_categories_name ON Categories(name);
